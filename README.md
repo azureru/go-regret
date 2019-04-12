b459d2750ba3dac853c3c0b98b6c128088e25e92
azureru <eru.saputra@gmail.com>
2019-04-13 01:21:41 +0700

    Initial code to connect and verify the username

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 This is a CLI tool to remove social media posts (twitter) -
 to encourage people to live in the moment and quickly move on from the past
 (and as we are getting older - to remove things that we may regret when we were younger)
+
+## Usage
+- You need to get customer key, secret, oauth token and oauth secret
+- Copy `config.sample.yaml` to `config.yaml`
+- Edit `config.yaml` and fill the appropriate fields
+- Run the cli `go-regret`
+
+## Clean 
+Will clean tweet that are older than N number of days (by default 30 days)
+This will use twitter search (so the max number of tweet are limited)
+
+## Purge
+Will clean tweets that listed on `tweet.js` (you can get this js on Twitter Archive Takeout)
--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Config struct {
+	ConsumerKey string `yaml:"consumer_key"`
+	ConsumerSecret string `yaml:"consumer_secret"`
+	OauthToken string `yaml:"oauth_token"`
+	OauthTokenSecret string `yaml:"oauth_token_secret"`
+}
+
 func main() {
 
+	flag.Parse()
+
+	var configStruct Config
+
+	// read config first
+	buffer, err := ioutil.ReadFile("./config.yaml")
+	if err != nil {
+		panic("./config.yaml is needed!")
+	}
+	err = yaml.Unmarshal(buffer, &configStruct)
+	if err != nil {
+		panic("invalid ./config.yaml file")
+	}
+
+	// connect
+	config := oauth1.NewConfig(configStruct.ConsumerKey, configStruct.ConsumerSecret)
+	token := oauth1.NewToken(configStruct.OauthToken, configStruct.OauthTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	// Verify Credentials
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		panic("Invalid credentials, please check ./config.yaml value")
+	}
+	fmt.Printf("Your Account:\nID: %+v\nHandle: @%+v\nName: %+v\n", user.ID, user.ScreenName, user.Name)
+
+	// the whole thing
 }
